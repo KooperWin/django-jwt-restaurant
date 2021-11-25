2eef28a3565fa673e54f1f4cefc348b24f7fada6
KooperWin <jesus.silva@potros.itson.edu.mx>
2021-11-25 16:35:10 +0000

    newnew

--- ristorante/tests.py
+++ ristorante/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from .models import *
 
-class JobModelTests(TestCase):
-	def test_job_name(self):
-		new_job = Job(name='My job')
-		print(new_job)
-		self.assertIs(str(new_job), 'My job')
+class RestaurantModelTests(TestCase):
+	def test_ristorante_name(self):
+		new_restaurant = Restaurant(name='My Restaurant')
+		print(new_restaurant)
+		self.assertIs(str(new_restaurant), 'My Restaurant')
